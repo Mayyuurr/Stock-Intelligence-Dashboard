58be5b7b104b7a06d17046affc905675236a7050
Mayur <mayurrathodavni@gmail.com>
2026-04-02 22:17:26 +0530

    Dployed Scripts

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://stock-intelligence-dashboard-lgdh.onrender.com" | "http://127.0.0.1:8000";
+const API_BASE = "https://stock-intelligence-dashboard-lgdh.onrender.com";
 let currentChart = null;
 
 // Initialize dashboard on load
